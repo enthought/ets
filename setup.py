019e41f492a3618cd6269e3c50e77aeb7a7207f4
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-08 20:55:12 +0000

    * Minor version bump after branching for release.

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
         ],
     test_suite = 'nose.collector',
     url = 'http://code.enthought.com/projects/tool-suite.php',
-    version = '3.0.0',
+    version = '3.0.1',
     zip_safe = True,
     )
 
