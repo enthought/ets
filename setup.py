4cb5bf3a3538d5108e2828a232522d73ae418930
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2011-01-26 21:32:56 +0000

    update version data in ETS meta project

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2008-2010 by Enthought, Inc.
+# Copyright (c) 2008-2011 by Enthought, Inc.
 # All rights reserved.
 
 """
@@ -20,33 +20,9 @@ from setuptools import setup
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
 
-
-# Function to convert simple ETS component names and versions to a requirements
-# spec that works for both development builds and stable builds.
-def build_reqs(list):
-    return ['%s >=%s.dev, <=%s' % (p, v, v) for p,v in list]
-
-# Declare the ETS component versions
-install_requires = build_reqs([
-    ('AppTools', '3.4.0'),
-    ('BlockCanvas', '3.2.0'),
-    ('Chaco', '3.3.2'),
-    ('CodeTools', '3.1.2'),
-    ('Enable', '3.3.2'),
-    ('EnthoughtBase', '3.0.6'),
-    ('EnvisageCore', '3.1.3'),
-    ('EnvisagePlugins', '3.1.3'),
-    ('ETSDevTools', '3.1.0'),
-    ('Mayavi', '3.4.0'),
-    ('SciMath', '3.0.5'),
-    ('Traits', '3.5.0'),
-    ('TraitsBackendQt', '3.5.0'),
-    ('TraitsBackendWX', '3.5.0'),
-    ('TraitsGUI', '3.5.0'),
-])
-
-INFO = {}
-INFO['version'] = '3.5.1'
+setup_data = dict(__name__='', __file__='setup_data.py')
+execfile('setup_data.py', setup_data)
+INFO = setup_data['INFO']
 
 # The actual setup call.
 setup(
@@ -72,28 +48,7 @@ setup(
         Topic :: Software Development :: Libraries
         """.splitlines() if len(c.strip()) > 0],
     description = DOCLINES[1],
-    extras_require = {
-        # Allow users to decide if they install ETS without external
-        # dependencies
-        'nonets': [
-            'AppTools[nonets]',
-            'BlockCanvas[nonets]',
-            'Chaco[nonets]',
-            'CodeTools[nonets]',
-            'Enable[nonets]',
-            'EnthoughtBase[nonets]',
-            'EnvisageCore[nonets]',
-            'EnvisagePlugins[nonets]',
-            'ETSDevTools[nonets]',
-            'Mayavi[nonets]',
-            'SciMath[nonets]',
-            'Traits[nonets]',
-            'TraitsBackendQt[nonets]',
-            'TraitsBackendWX[nonets]',
-            'TraitsGUI[nonets]'
-            ],
-        },
-    install_requires = install_requires,
+    install_requires = INFO['install_requires'],
     license = 'BSD',
     long_description = '\n'.join(DOCLINES[3:]),
     maintainer = 'ETS Developers',
---
+++ setup_data.py
@@ -0,0 +1,23 @@
+requirements = [
+    ('AppTools', '3.4.1'),
+    ('BlockCanvas', '3.2.1'),
+    ('Chaco', '3.4.0'),
+    ('CodeTools', '3.2.0'),
+    ('Enable', '3.4.0'),
+    ('EnthoughtBase', '3.1.0'),
+    ('EnvisageCore', '3.2.0'),
+    ('EnvisagePlugins', '3.2.0'),
+    ('ETSDevTools', '3.1.1'),
+    ('Mayavi', '3.4.1'),
+    ('SciMath', '3.0.7'),
+    ('Traits', '3.6.0'),
+    ('TraitsBackendQt', '3.6.0'),
+    ('TraitsBackendWX', '3.6.0'),
+    ('TraitsGUI', '3.6.0'),
+]
+
+INFO = {
+    'name': 'ETS',
+    'version': '3.6.0',
+    'install_requires': ['%s >= %s.dev' % nv for nv in requirements],
+}
