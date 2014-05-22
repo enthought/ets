d93a02d15cf5297e5ccfb2b02b938bcc36645355
David Cournapeau <cournape@gmail.com>
2014-05-22 01:47:31 -0700

    MAINT: update to latest ets versions.

--- setup_data.py
+++ setup_data.py
@@ -1,25 +1,25 @@
 requirements = [
-    ('apptools', '4.2.0'),
+    ('apptools', '4.2.1'),
     ('blockcanvas', '4.0.3'),
     ('casuarius', '1.1'),
-    ('chaco', '4.4.0'),
+    ('chaco', '4.4.1'),
     ('codetools', '4.2.0'),
-    ('enable', '4.3.0'),
-    ('enaml', '0.6.8'),
-    ('encore', '0.4.0'),
+    ('enable', '4.4.1'),
+    ('enaml', '0.9.5'),
+    ('encore', '0.5.1'),
     ('envisage', '4.4.0'),
     ('etsdevtools', '4.0.2'),
     ('etsproxy', '0.1.2'),
     ('graphcanvas', '4.0.2'),
-    ('mayavi', '4.3.0'),
+    ('mayavi', '4.3.1'),
     ('pyface', '4.4.0'),
     ('scimath', '4.1.2'),
-    ('traits', '4.4.0'),
+    ('traits', '4.5.0'),
     ('traitsui', '4.4.0'),
 ]
 
 INFO = {
     'name': 'ets',
-    'version': '4.4.0',
+    'version': '4.4.2',
     'install_requires': ['%s >= %s.dev' % nv for nv in requirements],
 }
